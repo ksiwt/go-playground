34aab11503fd629bcca772a98e76d24ef9b099d9
bkds <bkdsjp@gmail.com>
2020-11-05 23:57:37 +0900

    add forget files:

--- try_db_sql/main.go
+++ try_db_sql/main.go
@@ -14,7 +14,7 @@ import (
 func main() {
 	db, err := sql.Open("mysql",
 		"root:mysql@tcp(127.0.0.1:43306)/test",
-		)
+	)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,7 +24,6 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
 	repo := repository.NewRepository(db)
 
 	ctx := context.Background()
